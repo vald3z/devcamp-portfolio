eb9c4b7bcc60db4d175dd43c69b4eea45ff28325
vald3z <vald3z.cat@gmail.com>
2017-06-19 17:27:33 +0000

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+      title: "My blog post #{blog}",
+      body: 'Bacon ipsum dolor amet capicola ham rump ribeye corned beef bacon. T-bone cow ham jowl, burgdoggen kielbasa porchetta frankfurter fatback pork belly pork chop. Rump beef turducken tri-tip salami. Tri-tip pancetta short loin meatloaf, beef chicken shoulder pork belly meatball landjaeger fatback. Filet mignon flank landjaeger boudin meatball, pig rump beef. Jowl prosciutto shoulder, ball tip pork belly rump shankle pork chop boudin corned beef ground round beef tail biltong. Strip steak doner cupim ham ground round kevin drumstick flank.'
+    )
+end
+
+puts '10 blog posts created'
+
+5.times do |skill|
+  Skill.create!(
+      title: "Rails #{skill}",
+      percent_utilized: 15
+    )
+end
+
+puts '5 skills created'
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Here is a subtitle",
+        body: 'Tail burgdoggen ribeye landjaeger turkey. Shankle tenderloin shank kielbasa brisket, sausage tail doner ham short loin alcatra hamburger. Chicken pig brisket turkey beef short loin pork loin, andouille frankfurter corned beef venison. Kielbasa corned beef prosciutto pastrami meatloaf short loin kevin. Drumstick ball tip pig tongue rump. Tongue burgdoggen ham brisket, pork chop meatball jowl cow boudin.',
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+      )
+end
+
+puts '9 portfolio items created'
+  
