46e21b2ffc90dabeaac5a9bd5f403046f3dcd78e
vald3z <vald3z.cat@gmail.com>
2017-06-22 17:08:11 +0000

    Implemented custom scopes for portfolio items;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
       title: "My blog post #{blog}",
-      body: 'Bacon ipsum dolor amet capicola ham rump ribeye corned beef bacon. T-bone cow ham jowl, burgdoggen kielbasa porchetta frankfurter fatback pork belly pork chop. Rump beef turducken tri-tip salami. Tri-tip pancetta short loin meatloaf, beef chicken shoulder pork belly meatball landjaeger fatback. Filet mignon flank landjaeger boudin meatball, pig rump beef. Jowl prosciutto shoulder, ball tip pork belly rump shankle pork chop boudin corned beef ground round beef tail biltong. Strip steak doner cupim ham ground round kevin drumstick flank.'
+      body: "Bacon ipsum dolor amet capicola ham rump ribeye corned beef bacon. T-bone cow ham jowl, burgdoggen kielbasa porchetta frankfurter fatback pork belly pork chop. Rump beef turducken tri-tip salami. Tri-tip pancetta short loin meatloaf, beef chicken shoulder pork belly meatball landjaeger fatback. Filet mignon flank landjaeger boudin meatball, pig rump beef. Jowl prosciutto shoulder, ball tip pork belly rump shankle pork chop boudin corned beef ground round beef tail biltong. Strip steak doner cupim ham ground round kevin drumstick flank.",
+      topic_id: Topic.last.id
     )
 end
 
@@ -16,11 +25,21 @@ end
 
 puts '5 skills created'
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "Here is a subtitle",
-        body: 'Tail burgdoggen ribeye landjaeger turkey. Shankle tenderloin shank kielbasa brisket, sausage tail doner ham short loin alcatra hamburger. Chicken pig brisket turkey beef short loin pork loin, andouille frankfurter corned beef venison. Kielbasa corned beef prosciutto pastrami meatloaf short loin kevin. Drumstick ball tip pig tongue rump. Tongue burgdoggen ham brisket, pork chop meatball jowl cow boudin.',
+        subtitle: "Ruby on Rails",
+        body: "Tail burgdoggen ribeye landjaeger turkey. Shankle tenderloin shank kielbasa brisket, sausage tail doner ham short loin alcatra hamburger. Chicken pig brisket turkey beef short loin pork loin, andouille frankfurter corned beef venison. Kielbasa corned beef prosciutto pastrami meatloaf short loin kevin. Drumstick ball tip pig tongue rump. Tongue burgdoggen ham brisket, pork chop meatball jowl cow boudin.",
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+      )
+end
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
+        body: "Tail burgdoggen ribeye landjaeger turkey. Shankle tenderloin shank kielbasa brisket, sausage tail doner ham short loin alcatra hamburger. Chicken pig brisket turkey beef short loin pork loin, andouille frankfurter corned beef venison. Kielbasa corned beef prosciutto pastrami meatloaf short loin kevin. Drumstick ball tip pig tongue rump. Tongue burgdoggen ham brisket, pork chop meatball jowl cow boudin.",
         main_image: "http://placehold.it/600x400",
         thumb_image: "http://placehold.it/350x200"
       )
