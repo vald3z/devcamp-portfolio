cfb1545c14d30fa580a2a32e9092c5ba7bfab319
vald3z <vald3z.cat@gmail.com>
2017-06-26 15:41:32 +0000

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,10 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+ 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
-  
   def self.angular
    where(subtitle: 'Angular')
   end
