4703b313553b671f05fea9cdb509439907cea7b3
vald3z <vald3z.cat@gmail.com>
2017-06-29 16:50:28 +0000

    (FAILED TO) Implement partials with local data for portfolio form and nav bars - the 'name' section duplicates when I try to update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,71 +4,69 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
+     3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
-      @portfolio_item = Portfolio.new(portfolios_params)
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'YAY YOU JUST MADE A PORTFOLIO!' }
-        format.json { render :show, status: :created, location: @portfolio_item }
-
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-  
+
   def show
-  @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
-  
-   def destroy
-     # Perform the lookup
-     @portfolio_item = Portfolio.find(params[:id])
-     
-     # Destroy/delete the record
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Record was successfully BANISHED.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-   end
-  
-  private
-  
-  def portfolios_params
-    params.require(:portfolio).permit(:title,
-                                      :subtitle, 
-                                      :body, 
-                                      technologies_attributes: [:name]
-                                      )
   end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
 end
