3d7af187cd41161e940cc4f5f2e3cb579c7b57dd
vald3z <vald3z.cat@gmail.com>
2017-07-31 17:10:35 +0000

    Implemented custom scope for recent blogs and hide draft posts from nom-admins

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_title = "My Portfolio Blog"
   end
 
@@ -15,7 +19,7 @@ class BlogsController < ApplicationController
   def show
     @blog = Blog.includes(:comments).friendly.find(params[:id])
     @comment = Comment.new
-    
+
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
